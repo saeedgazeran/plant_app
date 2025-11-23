fcd4603c03dd0feeeaf8487fe5e9acb4eefac4b3
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-11-23 02:51:39 -0800

    Edit Colors and Buttons

--- lib/const/constants.dart
+++ lib/const/constants.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static Color primaryColor = Colors.green;
+  static Color primaryColor = Color(0XFF296E48);
   static Color blackColor = Colors.black;
 
   static String titleOne = "گیاهان را بهتر از قبل درک کن";
-  static String descriptionOne = "در مورد نگه داری گل و گیاهان میتونی اطلاعات کسب کنی";
+  static String descriptionOne =
+      "در مورد نگه داری گل و گیاهان میتونی اطلاعات کسب کنی";
   static String titleTwo = "با گیاهان جدید آشنا شو";
-  static String descriptionTwo = "رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی";
+  static String descriptionTwo =
+      "رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی";
   static String titleThree = "با یک گل بهار نمیشود، گل بکار";
-  static String descriptionThree = "هر گلی نیاز داشته باشید در این اپلیکیشن پیدا میکنید";
+  static String descriptionThree =
+      "هر گلی نیاز داشته باشید در این اپلیکیشن پیدا میکنید";
 }
-  
