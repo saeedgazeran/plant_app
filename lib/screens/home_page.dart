1c1274457c7f75a83c608226d068f196be6e8c49
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-11-23 02:40:58 -0800

    Edit Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,30 +12,36 @@ class _HomePageState extends State<HomePage> {
   final PageController _pageController = PageController(initialPage: 0);
   int currentIndex = 0;
 
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(microseconds: 300),
+      height: 10,
+      width: isActive ? 20 : 8.0,
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        color: Constants.primaryColor,
+        borderRadius: BorderRadius.circular(40),
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < 3; i++) {
+      if (currentIndex == i) {
+        indicators.add(_indicator(true));
+      } else {
+        indicators.add(_indicator(false));
+      }
+    }
+    return indicators;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 20, right: 20),
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                "رد کردن",
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontFamily: "lalezar",
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0.0),
       body: Stack(
         children: <Widget>[
           PageView(
@@ -63,6 +69,48 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
+          Positioned(
+            bottom: 80,
+            left: 30,
+            child: Row(children: _buildIndicator()),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 30,
+            child: Container(
+              padding: EdgeInsets.all(4),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.primaryColor,
+              ),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentIndex < 2) {
+                      currentIndex++;
+                      if (currentIndex < 3) {
+                        _pageController.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      }
+                    } else {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    }
+                  });
+                },
+                icon: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                  size: 25,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -84,7 +132,7 @@ class CreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 50, right: 50),
+      padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -111,8 +159,18 @@ class CreatePage extends StatelessWidget {
               color: Colors.grey,
             ),
           ),
+          SizedBox(height: 20),
         ],
       ),
     );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
