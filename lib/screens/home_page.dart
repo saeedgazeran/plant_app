2e6d9ac3c6c1049d6b160723889ca26ef22b3369
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-11-26 14:58:16 -0800

    Edit Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/const/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +9,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  final List<String> _plantTypes = [
+    '| پیشنهادی |',
+    '| آپارتمانی |',
+    '| محل‌کار |',
+    '| گل باغچه‌ایی |',
+    '| گل سمی |',
+  ];
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(child: Text("صفحه اصلی"),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // Search Box
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  width: size.width * 0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Constants.primaryColor.withValues(alpha: 0.1),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.mic,
+                        color: Constants.blackColor.withValues(alpha: 0.6),
+                      ),
+                      Expanded(
+                        child: Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextField(
+                            cursorColor: Constants.primaryColor,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(right: 5),
+                              hintText: "جستجو ...",
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontFamily: "iranSans",
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Constants.blackColor.withValues(alpha: 0.6),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Category
+            
+
+          ],
+        ),
+      ),
     );
   }
 }
