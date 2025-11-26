49b8ce0cee6501bbd7acd53aacf6706b0645a9ab
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-11-26 14:09:31 -0800

    finished root page

--- lib/screens/root.dart
+++ lib/screens/root.dart
@@ -4,6 +4,9 @@ import 'package:plant_app/screens/cart_page.dart';
 import 'package:plant_app/screens/favorite_page.dart';
 import 'package:plant_app/screens/home_page.dart';
 import 'package:plant_app/screens/profile_page.dart';
+import 'package:plant_app/screens/scan_page.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:page_transition/page_transition.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -21,7 +24,7 @@ class _RootPageState extends State<RootPage> {
     Icons.home,
     Icons.favorite,
     Icons.shopping_cart,
-    Icons.people,
+    Icons.person,
   ];
 
   List<String> appBarTitle = ["خانه", "علاقه‌مندی‌ها", "سبد‌خرید", "پروفایل"];
@@ -43,12 +46,45 @@ class _RootPageState extends State<RootPage> {
                 style: TextStyle(
                   color: Constants.blackColor,
                   fontFamily: "Lalezar",
-                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
                 ),
               ),
             ],
           ),
         ),
+      ),
+      body: IndexedStack(index: bottomIndex, children: page),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            PageTransition(
+              type: PageTransitionType.bottomToTop,
+              child: ScanPage(),
+            ),
+          );
+        },
+        backgroundColor: Constants.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(50.0),
+        ),
+        child: Image.asset("assets/images/code-scan-two.png", height: 30.0),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        activeIndex: bottomIndex,
+        inactiveColor: Colors.black.withValues(alpha: 0.5),
+        gapLocation: GapLocation.center,
+        icons: iconList,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index) {
+          setState(() {
+            bottomIndex = index;
+          });
+        },
       ),
     );
   }
