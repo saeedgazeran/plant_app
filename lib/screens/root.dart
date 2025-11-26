18b0fe1ef7c12413c5eca2dd020e8b6146482af8
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-11-26 02:17:23 -0800

    edit root

--- lib/screens/root.dart
+++ lib/screens/root.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/const/constants.dart';
+import 'package:plant_app/screens/cart_page.dart';
+import 'package:plant_app/screens/favorite_page.dart';
+import 'package:plant_app/screens/home_page.dart';
+import 'package:plant_app/screens/profile_page.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -8,11 +13,42 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int bottomIndex = 0;
+
+  List<Widget> page = [HomePage(), FavoritePage(), CartPage(), ProfilePage()];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.people,
+  ];
+
+  List<String> appBarTitle = ["خانه", "علاقه‌مندی‌ها", "سبد‌خرید", "پروفایل"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Root Page"),
+        elevation: 0.0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.notifications, color: Constants.blackColor, size: 30),
+              Text(
+                appBarTitle[bottomIndex],
+                style: TextStyle(
+                  color: Constants.blackColor,
+                  fontFamily: "Lalezar",
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
