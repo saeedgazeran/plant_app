47bf22fa31407f78dbf2e563376a0a7904e06dc8
saeedgazeran <saeed.gazeran1383@gmail.com>
2025-12-08 09:29:54 -0800

    Edit ScanPage

--- lib/screens/scan_page.dart
+++ lib/screens/scan_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/const/constants.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -10,8 +11,76 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(child: Text("علف اسکن کن"),),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 50.0,
+            left: 20.0,
+            right: 20.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // X Button
+                  child: Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Constants.primaryColor.withValues(alpha: 0.15),
+                    ),
+                    child: Icon(Icons.close, color: Constants.primaryColor),
+                  ),
+                ),
+                // Like Button
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Constants.primaryColor.withValues(alpha: 0.15),
+                  ),
+                  child: Icon(Icons.share, color: Constants.primaryColor),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            top: 100,
+            left: 20,
+            right: 20,
+            child: SizedBox(
+              width: size.width * 0.8,
+              height: size.height * 0.8,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("assets/images/code-scan.png", height: 100),
+                    SizedBox(height: 20),
+                    Text(
+                      "برای اسکن گیاه کلیک کنید",
+                      style: TextStyle(
+                        color: Constants.primaryColor,
+                        fontFamily: "Lalezar",
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
